537254bbb44f191a5dfe1c1fe0cc92fe6441ffbf
kevin <kevin@joone.fr>
2020-10-13 08:26:20 +0200

    hook

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,33 +1,18 @@
 import React from 'react'
 
 
-class SearchBar extends React.Component {
-    constructor (props) {
-        super (props)
-        this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
-        this.handleInStockOnlyChange = this.handleInStockOnlyChange.bind(this)
-    }
+function SearchBar (props) {
+    const {filterText, onFilterTextChange, onStockChange, inStockOnly} = props
 
-    handleFilterTextChange(e) {
-        this.props.onFilterTextChange(e.target.value)
-    }
-
-    handleInStockOnlyChange(e) {
-        this.props.onStockChange(e.target.checked)
-    }
-    
-    render() {
-        const {filterText, inStockOnly} = this.props
-        return <div>
-            <div>
-                <input type="text" value={filterText} placeholder="Rechercher" onChange={this.handleFilterTextChange}/>
-            </div>
-            <div>
-                <input type="checkbox" id="stock" checked={inStockOnly} onChange={this.handleInStockOnlyChange}/>
-                <label htmlFor="stock">Produit en stock</label>
-            </div>
+    return (<div>
+        <div>
+            <input type="text" value={filterText} placeholder="Rechercher" onChange={(e) => onFilterTextChange(e)}/>
         </div>
-    } 
+        <div>
+            <input type="checkbox" id="stock" checked={inStockOnly} onChange={(e) => onStockChange(e)}/>
+            <label htmlFor="stock">Produit en stock</label>
+        </div>
+    </div>)
 }
 
 export default SearchBar;
