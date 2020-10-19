32f4e67755edee3210b4a772dbfe545ac4c88cdd
Julie Blg <julie.b@joone.fr>
2020-10-19 15:44:46 +0200

    Solution

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -1,29 +1,21 @@
 import React, { useState } from 'react'
-import ProductTable from './ProductTable.js' 
+import uniq from 'lodash/uniq'
+
 import SearchBar from './SearchBar.js'
-import ProductRow from './ProductRow.js'
 import './FilterableProductTable.css'
 
-function FilterableProductTable ({productsObject}) {
+const FilterableProductTable = ({productsObject}) => {
     const [filterText, setText] = useState("")
     const [inStockOnly, setStock] = useState(false)
     const [products, setProducts] = useState(productsObject)
-    
-    let tableProducts = []
-    
-    const handleInFilterText = function (e) {
-        setText (e.target.value)
-    }
 
-    const handleInStockOnlyChange = function (e) {
-        setStock (e.target.checked)
-    }
+    const categories = uniq(productsObject.map(product => product.category))
 
-   const handleFilterChange = function (e) {
+    const handleFilterChange = function (e) {
         const result = productsObject.filter(product => {
             if (e.currentTarget.type === "checkbox")
             {
-                handleInStockOnlyChange(e)
+                setStock(e.target.checked)
                 if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1)
                     return false
                 if (e.target.checked && !product.stocked)
@@ -31,7 +23,7 @@ function FilterableProductTable ({productsObject}) {
             }
             else
             {
-                handleInFilterText(e)
+                setText(e.target.value)
                 if (product.name.toLowerCase().indexOf(e.target.value.toLowerCase()) === -1)
                     return false
                 if (inStockOnly && !product.stocked)
@@ -42,54 +34,27 @@ function FilterableProductTable ({productsObject}) {
         setProducts(result)
     }
 
-    const indexProducts = function () {
-        const categoriesAndProduct = []
-        let lastCategoriy = []
-    
-        products.map((product, index) => {
-                if (!lastCategoriy.includes(product.category)){
-                    lastCategoriy.push(product.category)
-                    categoriesAndProduct[product.category] = []
-                }
-                const name = product.stocked ? product.name : <span className='danger'>{product.name}</span>
-                categoriesAndProduct[product.category].push(
-                    <ProductRow 
-                        key={index} 
-                        name={name}
-                        price={product.price }
-                    />
-                )
-            return categoriesAndProduct
-        })
-        return categoriesAndProduct
-    }
-
-    const createTableProducts = function (categoriesAndProduct) {
-        const table = []
-
-        for (const property in categoriesAndProduct) {
-                    table.push(
-                        <ProductTable 
-                            category = {property}
-                            key={property}
-                            rows={categoriesAndProduct[property]}
-                        />
-                    )
-         }
-         return table
-    }
-
-    tableProducts = createTableProducts(indexProducts())
-
-    return (<>
-        <SearchBar 
-            filterText={filterText} 
-            onFilterTextChange={handleFilterChange}
-            inStockOnly={inStockOnly}
-            onStockChange={handleFilterChange}
-        />
-        {tableProducts}
-    </>)
+    return (
+        <>
+            <SearchBar 
+                filterText={filterText} 
+                onFilterTextChange={handleFilterChange}
+                inStockOnly={inStockOnly}
+                onStockChange={handleFilterChange}
+            />
+            {categories.map(category => 
+                <>
+                    <p>{category}</p>
+                    {products.filter(product => product.category === category).map(product => 
+                        <div>
+                            <p>{product.name}</p> 
+                            <p>{product.price}</p>
+                        </div>
+                    )}
+                </>
+            )}
+        </>
+    )
 }
 
 export default FilterableProductTable;
