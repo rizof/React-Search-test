e83899657924760b0a9c39638a6ea70f171c24ee
cosmon <kevin@joone.fr>
2020-10-19 14:57:45 +0200

    return change

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -4,19 +4,11 @@ import ContentCart from './ContentCart'
 function ShoppingCart (props) {
     const {cart, delCart} = props
 
-    const indexCartProducts = function () {
-        const tableCart = []
-        cart.map((prod, index) => 
-            tableCart.push(<ContentCart key={index} prod={prod} delCart={() => delCart(prod)}/>)
-        )
-        return tableCart
-    }
-
-    let tableCart = indexCartProducts()
-
     return (
         <>
-            {tableCart}
+            {cart.map((prod, index) => 
+                <ContentCart key={index} prod={prod} delCart={() => delCart(prod)}/>
+            )}
         </>
     )
 }
